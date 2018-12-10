7bf2e992215f6fa83276d74644033b402f9eca8a
Abigail Lu <abigailitliong.lu@calbaptist.edu>
2018-12-09 16:03:29 -0800

    updated date

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 /**
  * Abigail Lu
  * CSC 513 - Final
- * Updated: 12/8/2018
+ * Updated: 12/9/2018
  * Description: The goals for this project can be found in the README.md file.
  * Test cases are found in MainTest.java.
  * Firewall is a package that contains AccessListController and Logger
